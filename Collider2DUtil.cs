519d35263cfa9d02f0f9a0f76d2469ac2fb993c0
Ted Aronson <ted.aronson@gmail.com>
2015-11-13 14:26:08 -0800

    util to create isotiles

---
+++ Collider2DUtil.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class Collider2DUtil
+{
+    [MenuItem("GameObject/2D Object/Isometric Tile")]
+    public static GameObject CreateIsometricTile()
+    {
+        GameObject go = new GameObject("IsoTile");
+
+        PolygonCollider2D polygon = go.AddComponent<PolygonCollider2D>();
+        float sqrt3 = Mathf.Sqrt(3f);
+        Vector2[] points = new Vector2[] { new Vector2(-sqrt3, 0f), new Vector2(0f, 1f), new Vector2(sqrt3, 0f), new Vector2(0f, -1f) };
+        polygon.SetPath(0, points);
+        Selection.activeGameObject = go;
+
+        return go;
+    }
+}
