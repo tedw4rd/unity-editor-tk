09dee5abdf9642c84c5fd58776842da4a8d2c3da
Ted Aronson <ted.aronson@gmail.com>
2015-11-12 22:33:11 -0800

    Util function to create a new asset

---
+++ ScriptableObjectUtil.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public static class ScriptableObjectUtil
+{
+    /// <summary>
+    /// Creates a new ScriptableObject of a given type and saves it to an asset at the path
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="path"></param>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static T CreateAsset<T>(string path, string name) where T : ScriptableObject
+    {
+        T obj = ScriptableObject.CreateInstance<T>();
+        AssetDatabase.CreateAsset(obj, Path.Combine(path, name + ".asset"));
+        AssetDatabase.SaveAssets();
+        return obj;
+    }
+}
