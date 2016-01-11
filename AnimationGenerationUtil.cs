bf62d05b7ef0a8c1fb327b039490f1778f133447
Ted Aronson <ted.aronson@gmail.com>
2016-01-10 21:03:09 -0800

    animation generation util file

---
+++ AnimationGenerationUtil.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public static class AnimationGenerationUtil
+{
+    public static AnimationCurve[] RotationAnimationCurves(Dictionary<float, Quaternion> keys)
+    {
+        AnimationCurve[] output = new AnimationCurve[4];
+        int size = keys.Count;
+        List<Keyframe>[] keyframes = new List<Keyframe>[4];
+        for (int i = 0; i < keyframes.Length; i++)
+        {
+            keyframes[i] = new List<Keyframe>();
+        }
+
+        foreach (KeyValuePair<float, Quaternion> keyValuePair in keys)
+        {
+            float time = keyValuePair.Key;
+            Quaternion rotation = keyValuePair.Value;
+            for (int i = 0; i < keyframes.Length; i++)
+            {
+                keyframes[i].Add(new Keyframe(time, rotation[i]));
+            }
+        }
+
+        for (int i = 0; i < keyframes.Length; i++)
+        {
+            output[i] = new AnimationCurve(keyframes[i].ToArray());
+        }
+
+        return output;
+    }
+}
